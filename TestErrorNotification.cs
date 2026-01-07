c2fa54635fcbb7d58eb904d3f0ccae92d1f448e8
Salvador Alfredo Zeledón <Salvador.Zeledon@ALMAPAC.COM>
2026-01-07 15:01:46 -0600

    test: agregar error intencional para probar notificaciones por email

---
+++ TestErrorNotification.cs
@@ -0,0 +1,14 @@
+// Este archivo tiene un error intencional para probar las notificaciones por email
+// BORRAR DESPUÉS DE PROBAR
+
+namespace Test
+{
+    public class TestError
+    {
+        // Error intencional: falta punto y coma
+        public void Test()
+        {
+            var x = 1
+        }
+    }
+}
